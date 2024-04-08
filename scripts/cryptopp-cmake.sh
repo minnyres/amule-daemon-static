c277f87cb6954ff27b08cda752f3da1e5a6a1a23
minnnyres <liuthgw23@gmail.com>
2024-04-08 15:19:43 +0000

    fix armv7 build

--- scripts/cryptopp-cmake.sh
+++ scripts/cryptopp-cmake.sh
@@ -4,7 +4,11 @@ set -e
 
 cd $SRCDIR/cryptopp-cmake
 
-cmake -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$BUILDDIR/cryptopp -DCRYPTOPP_BUILD_TESTING=OFF >> $LOG 2>&1
+if [ $TARGET == "armv7-linux-musleabihf" ]; then
+    extra_cmake_options="-DCRYPTOPP_DISABLE_ASM=ON"
+fi
+
+cmake -B build $extra_cmake_options -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$BUILDDIR/cryptopp -DCRYPTOPP_BUILD_TESTING=OFF >> $LOG 2>&1
 mkdir -p $BUILDDIR/cryptopp/lib
 ln -snf lib $BUILDDIR/cryptopp/lib64
 cmake --build build >> $LOG 2>&1
